6438a88c38dbb9c4f95ab23f9b65f54d3eb5c5da
Lucas <soares0lucas8@gmail.com>
2025-06-18 18:57:12 -0300

    feat: Injeção de Dependencia dos serviços

---
+++ MotoHub.CrossCutting/IoC/PipelineExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MotoHub.Application.Services;
+using MotoHub.Domain.Interfaces;
+
+namespace MotoHub.CrossCutting.IoC;
+
+public static class PipelineExtensions
+{
+    public static void AddApplicationDI(this IServiceCollection services)
+    {
+        services.AddScoped<IAdministradorService, AdministradorService>();
+        services.AddScoped<IDeliveryManService, DeliveryManService>();
+        services.AddScoped<IMotorcycleService, MotorcycleService>();
+        services.AddScoped<IRentalService, RentalService>();
+    }
+}
