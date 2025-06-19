04804f0c6470f51eccf247eb04e5e336478ab233
Lucas <soares0lucas8@gmail.com>
2025-06-19 06:55:48 -0300

    feat: validações com FluentValidation no AdministradorController

---
+++ MotoHub/Validators/Administrador/CreateAdministradorModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MotoHub.API.Models.Administrador;
+using MotoHub.API.Resources;
+
+namespace MotoHub.API.Validators.Administrador;
+
+public class CreateAdministradorModelValidator : AbstractValidator<CreateAdministradorModel>
+{
+    public CreateAdministradorModelValidator()
+    {
+        RuleFor(model => model.Identifier)
+            .NotEmpty()
+            .WithMessage(model => string.Format(ApiMessage.Require_Warning, nameof(model.Identifier)));
+
+        RuleFor(model => model.Password)
+            .NotEmpty()
+            .WithMessage(model => string.Format(ApiMessage.Require_Warning, nameof(model.Password)));
+    }
+}
---
+++ MotoHub/Validators/Administrador/LoginModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MotoHub.API.Models.Administrador;
+using MotoHub.API.Resources;
+
+namespace MotoHub.API.Validators.Administrador;
+
+public class LoginModelValidator : AbstractValidator<LoginModel>
+{
+    public LoginModelValidator()
+    {
+        RuleFor(model => model.Identifier)
+            .NotEmpty()
+            .WithMessage(model => string.Format(ApiMessage.Require_Warning, nameof(model.Identifier)));
+
+        RuleFor(model => model.Password)
+            .NotEmpty()
+            .WithMessage(model => string.Format(ApiMessage.Require_Warning, nameof(model.Password)));
+    }
+}
